24d451b6b02fa9cbbf9d554e47b534f182908337
AndreCMendess <andremendesdede2@gmail.com>
2024-10-03 15:41:02 -0300

    excecao personalizada para objeto nao encontrado

---
+++ src/main/java/com/andre/bookstore/exceptions/StandardError.java
@@ -0,0 +1,44 @@
+package com.andre.bookstore.exceptions;
+
+public class StandardError {
+
+    private long timestamp;
+    private Integer status;
+    private String error;
+
+    public StandardError() {
+        super();
+    }
+
+    public StandardError(String error, long timestamp, Integer status) {
+        super();
+        this.error = error;
+        this.timestamp = timestamp;
+        this.status = status;
+    }
+
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
--- src/main/java/com/andre/bookstore/service/CategoriaService.java
+++ src/main/java/com/andre/bookstore/service/CategoriaService.java
@@ -1,6 +1,7 @@
 package com.andre.bookstore.service;
 
 import com.andre.bookstore.domain.Categoria;
+import com.andre.bookstore.exceptions.ObjectNotFoundException;
 import com.andre.bookstore.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,7 @@ public class CategoriaService {
     public Categoria findById(Integer id){
 
         Optional<Categoria> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id: " + id + ", Tipo: " + Categoria.class.getName()));
 
     }
 
