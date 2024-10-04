1251bdcf8b60226394845f9ea363ba93a72378d4
AndreCMendess <andremendesdede2@gmail.com>
2024-10-03 21:20:46 -0300

    adicionados comentarios baseados no meu entendimento de cada linha do codigo

--- src/main/java/com/andre/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/andre/bookstore/resources/CategoriaResource.java
@@ -19,33 +19,42 @@ public class CategoriaResource {
     @Autowired
     private CategoriaService service;
 
-    @GetMapping(value="/{id}")
-    public ResponseEntity<Categoria> findById(@PathVariable Integer id){
+    @GetMapping(value="/{id}")// Define que ese metodo é uma requisição GET com um id na url
+    public ResponseEntity<Categoria> findById(@PathVariable Integer id){// Recebe o id como parametro e retorna a categoria correspondente
 
+      // Busca uma categoria no banco de dados pelo id informado.
       Categoria obj = service.findById(id);
+      // Retorna uma resposta com o status 200 (ok), e a categoria encontrada no corpo da resposta
       return  ResponseEntity.ok().body(obj);
     }
 
-    @GetMapping
-    public ResponseEntity<List<CategoriaDTO>> findAll(){
+    @GetMapping// Define que esse metodo é uma requisição Get
+    public ResponseEntity<List<CategoriaDTO>> findAll(){//Retorna uma lista de CategoriaDTO
+        // Obtém a lista  de todas as categorias do banco de dados
         List<Categoria> list = service.findAll();
+        // Converte cada objeto da lista de Categoria em CategoriaDTO usando stream
         List<CategoriaDTO> listDTO = list.stream().map(CategoriaDTO::new).collect(Collectors.toList());
+        // Retorna uma resposta com o status 200 (ok), e a lista de categorias (listDTO) no corpo da resposta
         return ResponseEntity.ok().body(listDTO);
     }
 
-    @PostMapping
-    public ResponseEntity<Categoria> create(@RequestBody Categoria obj){
+    @PostMapping// Define que este metodo é uma requisição post (create).
+    public ResponseEntity<Categoria> create(@RequestBody Categoria obj){//Recebe um objeto categoria na forma de um request json
+           //Cria a categoria no banco de dados e atualiza a variavel obj com ela
            obj = service.create(obj);
+           //Cria uma URI que aponta pro recurso criado , incluindo o id da categoria
            URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+           // retorna uma resposta com o status 201 (created) e a URI de um novo recurso criado
+           // Se fosse .body(obj) retornaria a resposta status 201 e o objeto Categoria ja criado no corpo da resposta
            return ResponseEntity.created(uri).build();
 
     }
 
-    @PutMapping(value = "/{id}")
-    public ResponseEntity<CategoriaDTO> update (@PathVariable Integer id,@RequestBody CategoriaDTO objDto){
+    @PutMapping(value = "/{id}")// Define que este metodo responde a requisições PUT na URL com um ID
+    public ResponseEntity<CategoriaDTO> update (@PathVariable Integer id,@RequestBody CategoriaDTO objDto){ // atualiza uma categoria com base no id e nos dados do objDto
 
-        Categoria newObj = service.create(id,objDto);
-        return ResponseEntity.ok().body(new CategoriaDTO(newObj));
+        Categoria newObj = service.create(id,objDto);// recebe o obj atualizado pelo serviço
+        return ResponseEntity.ok().body(new CategoriaDTO(newObj));// retorna a reposta http 200 com o corpo da resposta a categoria atualizada
     }
 
 
--- src/main/java/com/andre/bookstore/service/CategoriaService.java
+++ src/main/java/com/andre/bookstore/service/CategoriaService.java
@@ -16,27 +16,39 @@ public class CategoriaService {
     @Autowired
     public CategoriaRepository repository;
 
+    // Metodo que retorna uma categoria procurada por id
     public Categoria findById(Integer id){
 
+        // Busca a categoria pelo id no banco de dados
         Optional<Categoria> obj = repository.findById(id);
+        // Retorna a categoria ou lança uma exceção ObjectNotFoundException se não encontrada
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id: " + id + ", Tipo: " + Categoria.class.getName()));
 
     }
 
+    // Metodo que retorna todas as categorias do banco de dados
     public List<Categoria> findAll(){
       return  repository.findAll();
     }
 
+    // Metodo que cria uma categoria
     public Categoria create(Categoria obj){
+        // Garante que o id seja nulo , indicando que é um novo registro
         obj.setId(null);
+        // Salva a nova categoria no banco de dados e retorna a categoria salva
         return repository.save(obj);
     }
 
 
+    // Metodo que atualiza uma categoria no banco de dados
     public Categoria create(Integer id, CategoriaDTO objDto) {
+        // Verifica se a categoria  existe no banco de dados por ID
         Categoria obj = findById(id);
+        // Atualiza o nome da categoria com os dados fornecidos no DTO
         obj.setNome(objDto.getNome());
+        // Atualiza a descrição da categoria com os dados fornecidos no DTO
         obj.setDescricao(objDto.getDescricao());
+        // Salva as alterações no banco de dados e retorna a categoria atualizada
         return repository.save(obj);
 
     }
